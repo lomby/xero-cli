4f9f38ed1e1545495089b56004854a9496cb825b
lomby <neil.sherwood@gmail.com>
2020-07-30 17:39:28 +0100

    Cli implementation
    Added cli command functionality
    Tweaked invoice functions for use with cli
    Added invoice online links

--- accounts/invoices.go
+++ accounts/invoices.go
@@ -1,29 +1,55 @@
 package accounts
 
 import (
-	"fmt"
-	"net/http"
+	"errors"
 
 	"github.com/lomby/xero-cli/xeroclient"
 )
 
 // GetInvoice fetches a single invoice when provided with a Xero InvoiceID
-func GetInvoice(res http.ResponseWriter, req *http.Request) {
-
-	invoiceID := req.FormValue("id")
+func GetInvoice(invoiceID string) (string, error) {
 
 	if invoiceID == "" {
-		fmt.Println("Invoice ID not provided")
-		return
+		return "", errors.New("Invoice ID not provided")
 	}
 
 	r, code, err := xeroclient.NewRequest("GET", "https://api.xero.com/api.xro/2.0/Invoices/"+invoiceID, nil)
 
 	if err != nil || code != 200 {
-		fmt.Println(code, err, r)
-		return
+		return "", err
 	}
 
-	fmt.Println(code, r)
-	return
+	return r, nil
+}
+
+// GetInvoices fetches a all invoices when provided with a CustomerID
+func GetInvoices(contactID string) (string, error) {
+
+	if contactID == "" {
+		return "", errors.New("Contact ID not provided")
+	}
+
+	r, code, err := xeroclient.NewRequest("GET", "https://api.xero.com/api.xro/2.0/Invoices?ContactIDs="+contactID, nil)
+
+	if err != nil || code != 200 {
+		return "", err
+	}
+
+	return r, nil
+}
+
+// GetInvoiceLink fetches the online url for an invoice using the invoice id
+func GetInvoiceLink(invoiceID string) (string, error) {
+
+	if invoiceID == "" {
+		return "", errors.New("Invoice ID not provided")
+	}
+
+	r, code, err := xeroclient.NewRequest("GET", "https://api.xero.com/api.xro/2.0/Invoices/"+invoiceID+"/OnlineInvoice", nil)
+
+	if err != nil || code != 200 {
+		return "", err
+	}
+
+	return r, nil
 }
